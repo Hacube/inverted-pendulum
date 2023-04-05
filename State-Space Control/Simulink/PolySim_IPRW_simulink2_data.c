bf86f809d31f9f394af9db13ca7a55b46ca58c47
Jacob Amezquita <113970708+Hacube@users.noreply.github.com>
2023-04-04 20:37:30 -0700

    Create PolySim_IPRW_simulink2_data.c

---
+++ State-Space Control/Simulink/PolySim_IPRW_simulink2_data.c
@@ -0,0 +1,43 @@
+
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: PolySim_IPRW_simulink2_data.c
+ *
+ * Code generated for Simulink model 'PolySim_IPRW_simulink2'.
+ *
+ * Model version                  : 1.11
+ * Simulink Coder version         : 9.8 (R2022b) 13-May-2022
+ * C/C++ source code generated on : Tue Apr  4 15:26:28 2023
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives:
+ *    1. Execution efficiency
+ *    2. RAM efficiency
+ * Validation result: Not run
+ */
+
+#include "PolySim_IPRW_simulink2.h"
+
+/* Constant parameters (default storage) */
+const ConstP rtConstP = {
+  /* Expression: A
+   * Referenced by: '<S1>/A matrix'
+   */
+  { 0.0, 85.761237633623281, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0434503773912017,
+    -0.03099003001841619 },
+
+  /* Expression: B
+   * Referenced by: '<S1>/B Matrix'
+   */
+  { 0.0, -1.2659997491681976, 0.90294659300184132 }
+};
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
